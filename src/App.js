8b72fe44c30bbadccd128c669127fd8fcbcbbf01
YassineKasraoui <yassinekasraoui42@gmail.com>
2022-02-09 00:24:07 +0100

    refactor: :art: update theme for app.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React from 'react'
-
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom'
-
+import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 
+import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+import { AuthProvider } from './appState/Auth/AuthProvider'
 import { appRoutes } from './routes'
-import { RenderRouteUtil } from './routes/RouteRenderUtil';
-import { AuthProvider } from './appState/Auth/AuthProvider';
+import { RenderRouteUtil } from './routes/RouteRenderUtil'
+import { theme } from './theme'
 
 export default function App() {
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            {appRoutes.map(RenderRouteUtil)}
-          </Routes>
+          <Routes>{appRoutes.map(RenderRouteUtil)}</Routes>
         </BrowserRouter>
       </AuthProvider>
-    </React.Fragment>
-  );
+    </ThemeProvider>
+  )
 }
---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material'
+
+export const palette = {
+  type: 'light',
+  primary: {
+    main: '#2ca9e0'
+  },
+  secondary: {
+    main: '#FCB07E'
+  }
+}
+
+export const theme = createTheme({
+  palette
+})
